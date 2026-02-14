3eb02dba4c4110cf473dc69ee4812172b715f8ce
jevinparmar <jevinparmar110@gmail.com>
2026-02-15 00:36:14 +0530

    Delete college python/Assignment - 2 directory

--- college python/Assignment - 2/python_pr4.py
+++
@@ -1,7 +0,0 @@
-def simple_interest(p, t, r=5):
-    return (p * t * r) / 100
-
-principal = float(input("Principal amount: "))
-time = float(input("Time period: "))
-
-print("Simple Interest:", simple_interest(principal, time))
--- college python/Assignment - 2/python_pr5.py
+++
@@ -1,7 +0,0 @@
-def outer(value):
-    def inner(v):
-        return v * v
-    return inner(value)
-
-n = int(input("Enter a number: "))
-print("Result:", outer(n))
