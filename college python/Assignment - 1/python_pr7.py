1e65b195b132ffbf7affe8c555f0f9fec88b2319
jevinparmar <jevinparmar110@gmail.com>
2026-03-19 14:11:25 +0530

    Add comment to describe list operations in python_pr7.py
    Added a comment at the beginning of the script to describe its functionality.

--- college python/Assignment - 1/python_pr7.py
+++ college python/Assignment - 1/python_pr7.py
@@ -1,3 +1,5 @@
+# Program to perform various list operations like add, remove, update, slicing, append, extend, pop, reverse and sort
+
 list1 = ['physics', 'chemistry', 1997, 2000]
 list2 = [1, 2, 3, 4, 5, 6, 7]
 
