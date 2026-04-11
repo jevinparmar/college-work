5d759fdc4db2b7fd0ad4f1f20400232caca1e984
jevinparmar <jevinparmar110@gmail.com>
2026-04-11 20:34:36 +0530

    Delete college python/All Units Programs/Unit 1 directory

--- college python/All Units Programs/Unit 1/Program_11.py
+++
@@ -1,9 +0,0 @@
-# Program to find total of any number of arguments
-
-def total_numbers(*nums):
-    total = 0
-    for num in nums:
-        total += num
-    print("Total =", total)
-
-total_numbers(10, 20, 30, 40)
--- college python/All Units Programs/Unit 1/Program_6.py
+++
@@ -1,8 +0,0 @@
-# Program to print histogram using a list
-
-def histogram(data):
-    for num in data:
-        print("*" * num)
-
-numbers = [4, 9, 7]
-histogram(numbers)
